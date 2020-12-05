6bda8f6737fc998e391ac1984d575c820ba4ab4a
hiro220 <weeklyjump0928@gmail.com>
2020-12-05 19:41:04 +0900

    プログラム実行中の時間

---
+++ src/include/programTime.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+
+import pygame
+
+class Time:
+    def __init__(self):
+        self.stop_time = 0
+        self.pause_time = 0
+
+    def get_ticks(self):
+        return pygame.time.get_ticks() - self.pause_time
+
+    def stop(self):
+        self.stop_time = pygame.time.get_ticks()
+    
+    def restart(self):
+        self.pause_time += pygame.time.get_ticks() - self.stop_time
+
+time = Time()
+
+print("time")
