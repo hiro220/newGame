92780b6892aa3461c8a021d06eed25dadea2b678
hiro220 <weeklyjump0928@gmail.com>
2020-12-05 19:58:14 +0900

    ゲーム中に生成したタイマーをグループ化

--- src/game/game.py
+++ src/game/game.py
@@ -5,12 +5,17 @@ import pygame
 from pygame.locals import *
 
 from character.player import PlayerSample
+from include.timer import Timer
 
 class Game:
     def __init__(self, screen):
         self.clock = pygame.time.Clock()        # 時間管理用
         self.player = PlayerSample()
         self.exit = False
+
+        self.timers = pygame.sprite.Group()
+        Timer.containers = self.timers
+        
         self.do(screen)
 
     def do(self, screen):
@@ -24,6 +29,7 @@ class Game:
                 break
 
     def process(self):
+        self.timers.update()
         self.player.move()
         for event in pygame.event.get():
             if event.type == KEYDOWN:
