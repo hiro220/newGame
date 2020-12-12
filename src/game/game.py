8b7ccb557d62e4c49bed02ee5f1473f403dc9c65
batacosan <batasan1998@gmail.com>
2020-12-12 18:34:57 +0900

    壁オブジェクトを作成

--- src/game/game.py
+++ src/game/game.py
@@ -5,13 +5,22 @@ import pygame
 from pygame.locals import *
 
 from character.player import PlayerSample
+from objects.wall_object import WallObject
 
 class Game:
     def __init__(self, screen):
         self.clock = pygame.time.Clock()        # 時間管理用
         self.player = PlayerSample()
+        self.left_wall = pygame.Rect(-10,0,15,200)
+        self.right_wall = pygame.Rect(1155,0,5,200)
         self.exit = False
         self.do(screen)
+
+        self.wall_group = pygame.sprite.Group()      # オブジェクト[壁]のグループ 
+        WallObject.containers = self.wall_group
+
+        self.wall1 = WallObject("image/wall.jpg", 0, 0, 15, 200, 5)
+        self.wall2 = WallObject("image/wall.jpg", 0, 0, 1160, -15, 5)
 
     def do(self, screen):
         while True:
@@ -25,6 +34,7 @@ class Game:
 
     def process(self):
         self.player.move()
+        self.wall_group.update()
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 # キーボード入力
@@ -37,5 +47,8 @@ class Game:
     def draw(self, screen):
         screen.fill((255,255,255))
         self.player.draw(screen)
+        self.wall_group.draw()
+        pygame.draw.rect(screen, (0,0,255), self.left_wall)
+        pygame.draw.rect(screen, (0,0,255), self.right_wall)
 
     
