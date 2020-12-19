34e18095ec59bf23e53d4d4cef436f154bdb8296
hiro220 <weeklyjump0928@gmail.com>
2020-12-19 18:26:16 +0900

    画像ファイルのパス修正

--- src/objects/wall_object.py
+++ src/objects/wall_object.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 class WallObject(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
         pygame.sprite.Sprite.__init__(self, self.containers)
-        self.image = pygame.image.load("../image/wall.jpg").convert_alpha()
+        self.image = pygame.image.load("image/wall.jpg").convert_alpha()
         self.width = width
         self.height = height
         self.rect = Rect(x, y, width, height)
