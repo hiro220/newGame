f7e0a11fd6341fc2f80eb4d8da00ccb7e0d2eea3
batacosan <batasan1998@gmail.com>
2021-02-13 12:26:09 +0900

    移動する床のバグを修正

--- src/objects/wall_object.py
+++ src/objects/wall_object.py
@@ -57,12 +57,43 @@ class WallObject(pygame.sprite.Sprite):
 class MovingFloor(WallObject):
     def __init__(self, x, y, width, height):
         super().__init__(x, y, width, height)
+        self.oldrect = self.rect.copy()
         self.dx = 5
         self.dy = 0
-    
+        
     def update(self, player, enemies):
         self.move(self.dx, self.dy)
+        if pygame.sprite.collide_rect(self, player):
+            print("aaa")
+            self.extrude_object(player)
+        collided_enemy = pygame.sprite.spritecollide(self, enemies, False)
+        if collided_enemy:
+            for enemy in collided_enemy:
+                self.extrude_object(enemy)
+        
         super().update(player, enemies)
     
+    def extrude_object(self, object):
+        xvectols = object.oldrect.centerx
+        xvectole = object.rect.centerx
+        yvectols = object.oldrect.centery
+        yvectole = object.rect.centery
+
+        xvecLen = xvectole - xvectols
+        if xvecLen > 0:
+            Pxvec = pygame.Rect(xvectols + (self.rect.width / 2), yvectols - (self.rect.height / 2), xvecLen, self.rect.height)
+        elif xvecLen < 0:
+            Pxvec = pygame.Rect(xvectole - (self.rect.width / 2), yvectols - (self.rect.height / 2), xvecLen * -1, self.rect.height)
+        else:
+            Pxvec = pygame.Rect(-100, -100, -100, -100)
+
+        if Pxvec.colliderect(object):
+            if xvecLen > 0:
+                xvecLen = self.rect.right - object.left
+            else:
+                xvecLen = self.rect.left - object.right
+            object.rect.move_ip(10, 0)
+
     def move(self, dx, dy):
+        self.oldrect = self.rect.copy()
         self.rect.move_ip(dx, dy)
