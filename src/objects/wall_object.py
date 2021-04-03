0731b1a6d41152baecbab452c5144f26ea91f127
batacosan <batasan1998@gmail.com>
2021-04-03 14:14:43 +0900

    wall_objectにコメント追加

--- src/objects/wall_object.py
+++ src/objects/wall_object.py
@@ -11,34 +11,46 @@ class WallObject(pygame.sprite.Sprite):
         
     def update(self, player, enemies):
         if pygame.sprite.collide_rect(self, player):
-            self.action(player)
+            self.action(player) # プレイヤーとの判定
         collided_enemy = pygame.sprite.spritecollide(self, enemies, False)
         if collided_enemy:
             for enemy in collided_enemy:
-                self.action(enemy)
+                self.action(enemy) # 敵キャラとの判定
 
     def action(self, object):
+        # 判定対象の移動ベクトルの始点・終点
         xvectols = object.oldrect.centerx
         xvectole = object.rect.centerx
         yvectols = object.oldrect.centery
         yvectole = object.rect.centery
 
+        # 移動ベクトルの大きさ
         xvecLen = xvectole - xvectols
         yvecLen = yvectole - yvectols
+
+        # x方向の移動ベクトル: 判定対象が右に移動
         if xvecLen > 0:
             Pxvec = pygame.Rect(xvectols + (object.rect.width / 2), yvectols - (object.rect.height / 2), xvecLen, object.rect.height)
+        # x方向の移動ベクトル: 判定対象が左に移動
         elif xvecLen < 0:
             Pxvec = pygame.Rect(xvectole - (object.rect.width / 2), yvectols - (object.rect.height / 2), xvecLen * -1, object.rect.height)
+        # x方向の移動ベクトル: 判定対象が移動していない
         else:
             Pxvec = pygame.Rect(-100, -100, -100, -100)
         
+        # y方向の移動ベクトル: 判定対象が下に移動
         if yvecLen > 0:
             Pyvec = pygame.Rect(xvectols - (object.rect.width / 2) , yvectols + (object.rect.height / 2), object.rect.width, yvecLen)
+        # y方向の移動ベクトル: 判定対象が上に移動
         elif yvecLen < 0:
             Pyvec = pygame.Rect(xvectols - (object.rect.width / 2), yvectole - (object.rect.height / 2), object.rect.width, yvecLen * -1)
+        # y方向の移動ベクトル: 判定対象が移動していない
         else:
             Pyvec = pygame.Rect(-100, -100, -100, -100)
 
+        # x方向の判定
+        # 判定対象が右方向へ移動: 壁の左側に押し出す
+        # 判定対象が左方向へ移動: 壁の右側に押し出す
         if Pxvec.colliderect(self.rect):
             if xvecLen > 0:
                 xvecLen = xvecLen - (self.rect.left - (xvectols + (object.rect.width / 2)))
@@ -46,6 +58,9 @@ class WallObject(pygame.sprite.Sprite):
                 xvecLen = xvecLen - (self.rect.right - (xvectols - (object.rect.width / 2)))
             object.rect.move_ip(-xvecLen, 0)
 
+        # y方向の判定
+        # 判定対象が下方向へ移動: 壁の下側へ押し出す
+        # 判定対象が上方向へ移動: 壁の上側へ押し出す
         if Pyvec.colliderect(self.rect):
             if yvecLen > 0:
                 yvecLen = yvecLen - (self.rect.top - (yvectols + (object.rect.height / 2)))
@@ -64,28 +79,36 @@ class MovingFloor(WallObject):
     def update(self, player, enemies):
         self.move(self.dx, self.dy)
         if pygame.sprite.collide_rect(self, player):
-            self.extrude_object(player)
+            self.extrude_object(player) # プレイヤーのと判定
         collided_enemy = pygame.sprite.spritecollide(self, enemies, False)
         if collided_enemy:
             for enemy in collided_enemy:
-                self.extrude_object(enemy)
+                self.extrude_object(enemy) # 敵キャラとの判定
         
         super().update(player, enemies)
     
     def extrude_object(self, object):
+        # 自オブジェクトの移動ベクトルの始点・終点
         xvectols = self.oldrect.centerx
         xvectole = self.rect.centerx
         yvectols = self.oldrect.centery
         yvectole = self.rect.centery
 
+        # 自オブジェクトのx方向移動ベクトルの大きさ
         xvecLen = xvectole - xvectols
+        # x方向の移動ベクトル: 判定対象が右に移動
         if xvecLen > 0:
             Pxvec = pygame.Rect(xvectols + (self.rect.width / 2), yvectols - (self.rect.height / 2), xvecLen, self.rect.height)
+        # x方向の移動ベクトル: 判定対象が左に移動
         elif xvecLen < 0:
             Pxvec = pygame.Rect(xvectole - (self.rect.width / 2), yvectols - (object.rect.height / 2), xvecLen * -1, self.rect.height)
+        # x方向の移動ベクトル: 判定対象が移動していない
         else:
             Pxvec = pygame.Rect(-100, -100, -100, -100)
 
+        # x方向の判定
+        # 自オブジェクトが右方向へ移動: 壁の右側に押し出す
+        # 自オブジェクトが左方向へ移動: 壁の左側に押し出す
         if Pxvec.colliderect(object):
             if xvecLen > 0:
                 xvecLen = self.rect.right - object.rect.left
