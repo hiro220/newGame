5196f4331f1fb182ea55ac33410eba591bc894a7
milledrian <millennium_myonn@yahoo.co.jp>
2021-06-19 12:10:49 +0900

    [ADD]敵キャラがプレイヤーとのあたり判定を測定するためのメソッド追加

--- src/character/enemy/exsample_enemy.py
+++ src/character/enemy/exsample_enemy.py
@@ -3,8 +3,8 @@ from pygame.locals import *
 from character.enemy.enemy_base import EnemyBase
 
 class EnemySample(EnemyBase):
-    def __init__(self):
-        super().__init__(10,"image/character/enemy.png",1)
+    def __init__(self, player):
+        super().__init__(10,"image/character/enemy.png",1, player)
         self.dx = 0
         self.dy = 0
         super().move(200, 100)
@@ -16,7 +16,11 @@ class EnemySample(EnemyBase):
 
         self.enemy_rule()
         super().move(self.dx,self.dy)
-        #print("x:",self.rect.x,"y:",self.rect.y)
+
+        if self.collision_detection():
+            print("プレイヤーと当たってます。")
+        else:
+            print("プレイヤーと当たっていません。")
 
     def enemy_rule(self):
         self.rule_flag += 1
