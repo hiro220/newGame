490e4b86ce9516855fc741d20a980c28db88156f
batacosan <batasan1998@gmail.com>
2021-07-03 14:50:43 +0900

    不具合修正

--- src/game/map/object_tab.py
+++ src/game/map/object_tab.py
@@ -1,3 +1,4 @@
+from common.objects_origin import ObjectsOrigin
 from include.map_config import GRID_SIZE
 import pygame
 from pygame.locals import *
@@ -14,19 +15,23 @@ class ObjectTab:
         self.button_dict = {}       # ボタンを格納するリスト 
         self.select_button = "-1"   # 選択中のボタンID
 
+        ObjectsOrigin.containers = pygame.sprite.Group()
+
+        self.setObjects()
+
     def showTab(self):
         # タブを表示
         pygame.draw.rect(self.screen, (255,0,0), (self.x, self.y, 100, 600))
 
     def setObjects(self):
         # オブジェクトに対応するボタンを生成
-        for i, key, value in enumerate(GameObject.items()):
+        for i, (key, value) in enumerate(GameObject.items()):
             obj = value(0, 0)
             self.button_dict[key] = Button(self.screen, self.x + 10, i * GRID_SIZE, obj.rect.width, obj.rect.height, obj.image)
 
     def updateClickedButton(self, event_list):
         # 選択中のオブジェクトを更新
-        for key, button in self.button.dict.items():
+        for key, button in self.button_dict.items():
             if button.isClicked(event_list):
                 self.select_button = key
 
